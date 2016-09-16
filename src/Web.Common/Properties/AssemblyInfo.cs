a70b448f6acf329301691958908acf916be35d85
Lukasz Jaskiewicz <dev@jaskiewicz.de>
2016-09-16 14:04:48 +0200

    project basic configuration

--- src/Web.Common/Properties/AssemblyInfo.cs
+++ src/Web.Common/Properties/AssemblyInfo.cs
@@ -1,19 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Capgemini")]
-[assembly: AssemblyProduct("Web.Common")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc5e0d64-3b8b-4ee6-995d-1bc27a3b102a")]
+﻿[assembly: System.Reflection.AssemblyTitleAttribute("Web.Common Library")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Web.Common Library")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2016 Lukasz Jaskiewicz. All Rights Reserved.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0")]
+[assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETStandard,Version=v1.6")]
