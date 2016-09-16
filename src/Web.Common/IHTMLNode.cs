4492e3db1ecb07c3753c39d81e3eb2566a402661
Lukasz Jaskiewicz <dev@jaskiewicz.de>
2016-09-16 22:22:35 +0200

    IHTMLNode interface added

---
+++ src/Web.Common/IHTMLNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Net.DevDone.Web.Common
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public interface IHTMLNode
+    {
+        void GetChildHTML(StringWriter strWriter);
+        void GetHTML(StringWriter strWriter);
+        HTMLNode NewChild(string tag);
+        HTMLNode NewChild(string tag, string val);
+        HTMLNode NewChild(string tag, string val, Dictionary<string, string> attrs);
+        void SetTag(string tag);
+        void SetVal(string val);
+    }
+}
