a2a72c6339fa2b3efb42d35178beeffd3bc8e156
Lukasz Jaskiewicz <dev@jaskiewicz.de>
2016-09-19 00:51:27 +0200

    HTMLTag and HTMLAttr classes added

---
+++ src/Web.Common/HTMLAttr.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2016 Lukasz Jaskiewicz. All Rights Reserved
+// Licenced under the European Union Public Licence 1.1 (EUPL v.1.1)
+// See licence.txt in the project root for licence information
+// Written by Lukasz Jaskiewicz (lukasz@jaskiewicz.de)
+
+namespace Net.DevDone.Web.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public static class HTMLAttr
+    {
+        public static readonly string class_ = "class";
+    }
+}
---
+++ src/Web.Common/HTMLTag.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2016 Lukasz Jaskiewicz. All Rights Reserved
+// Licenced under the European Union Public Licence 1.1 (EUPL v.1.1)
+// See licence.txt in the project root for licence information
+// Written by Lukasz Jaskiewicz (lukasz@jaskiewicz.de)
+
+namespace Net.DevDone.Web.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public static class HTMLTag
+    {
+        public static readonly string html = "html";
+        public static readonly string head = "head";
+        public static readonly string body = "body";
+        public static readonly string article = "article";
+        
+    }
+}
